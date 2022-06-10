aa2ec7e73d9334fbb2bb66f998596abc1fddf55e
Phenixel <yonathancardoso@outlook.fr>
2022-06-10 12:16:13 +0200

    (update) Best code (feat) get with date (fix) optimized

--- app_manager/views.py
+++ app_manager/views.py
@@ -1,35 +1,27 @@
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-from django.shortcuts import render
-from django.http import Http404
-
-from django.views.generic import TemplateView, ListView
-
+import datetime
+import email
+from email.header import Header, decode_header, make_header
+import imaplib
 import environ
+from django.views.generic import TemplateView
 
 from mail_manager.settings import BASE_DIR
-
 env = environ.Env()
 environ.Env.read_env(env_file=str(BASE_DIR / "mail_manager" / ".env"))
 
 ##########################
 # Récupération des mails #
 ##########################
-
-import imaplib
-import email
-
-server = "imap.gmail.com"
-imap = imaplib.IMAP4_SSL(server)
-
 username = env("USERMAIL")
 password = env("MDPMAIL")
 
-imap.login(username, password)
+m = imaplib.IMAP4_SSL("imap.gmail.com")
+m.login(username, password)
 
-res, messages = imap.select('"INBOX"')
+# Boite
+m.select("inbox")
+m.list()
 
-messages = int(messages[0])
 
 ##########################
 
@@ -37,34 +29,64 @@ messages = int(messages[0])
 class HomeAppView(TemplateView):
     template_name = "app_manager/index.html"
 
-    # A définir selon besoin
-    n = 10
-    attendu = 8
-
     # Déclarations des variables utiles. Ne pas toucher
-    ras = 0
-    erreur = 0
+    encoding = 'utf-8'
+    final_list = []
+    ras = erreur = 0
     lstSubjectRAS = []
     lstSubjectError = []
 
-    for i in range(messages, messages - n, -1):
-        res, msg = imap.fetch(str(i), "(RFC822)")
-        for response in msg:
-            if isinstance(response, tuple):
-                msg = email.message_from_bytes(response[1])
-                From = msg["From"]
-                subject = msg["Subject"]
-                pos = subject.find('serveur')
+    # Listes des mots de RAS
+    lst_RAS = [
+        "ras",
+        "réussi ",
+        "terminée",
+        "terminé"
+    ]
 
-                if "[RAS]" in subject:
-                    ras = ras + 1
-                    lstSubjectRAS.append(subject[pos:])
-                if "[ERREUR]" in subject:
-                    erreur = erreur + 1
-                    lstSubjectError.append(subject[pos:])
+    # Liste des mot d'erreurs
+    lst_ERREUR = [
+        "error",
+        "erreur",
+        "échoué"
+    ]
 
-    nbInconnu = n - (ras + erreur)
-    nbSI = attendu - (ras + erreur)
+    # Déclarations
+    attendu = 10
+    date = (datetime.date.today() - datetime.timedelta(30)).strftime("%d-%b-%Y")
+    msg_from = '"Yonathan Cardoso"'
+
+    # Tri
+    result, data = m.search(None, '(FROM {msg_from} SENTSINCE {date})'.format(date=date, msg_from=msg_from))
+    ids = str(data[0], encoding)
+
+    # Création d'une liste de message par ids
+    id_list = ids.split()
+
+    for emailid in id_list:
+        temp_dict = {}
+        result, data = m.fetch(str(emailid), "(RFC822)")
+        email_body = data[0][1]
+        mail = email.message_from_bytes(email_body)
+
+        temp_dict['Sender'] = mail["From"]
+        s = temp_dict['Subject'] = mail["Subject"]
+        temp_dict['Date'] = mail["Date"]
+        pos = temp_dict['Subject'].find('serveur')
+
+        if "[RAS]" in temp_dict['Subject']:
+            ras = ras + 1
+            lstSubjectRAS.append(temp_dict['Subject'][pos:])
+
+        if "[ERREUR]" in temp_dict['Subject']:
+            erreur = erreur + 1
+            lstSubjectError.append(temp_dict['Subject'][pos:])
+
+        # print(temp_dict)
+        final_list.append(temp_dict)
+
+    manque = attendu - (ras + erreur)
+    print(ras)
 
     def get_context_data(self, **kwargs):
         context = super(HomeAppView, self).get_context_data(**kwargs)
@@ -72,8 +94,7 @@ class HomeAppView(TemplateView):
             {
                 'ras': self.ras,
                 'erreur': self.erreur,
-                'nbInconnu': self.nbInconnu,
-                'nbSI': self.nbSI,
+                'manque': self.manque,
                 'lstSubjectError': self.lstSubjectError,
                 'lstSubjectRAS': self.lstSubjectRAS,
             }
@@ -83,41 +104,3 @@ class HomeAppView(TemplateView):
 
 class AppMoreInfos(TemplateView):
     template_name = "app_manager/more_infos.html"
-
-    # A définir selon besoin
-    n = 10
-    attendu = 8
-
-    # Déclarations des variables utiles. Ne pas toucher
-    lstSubjectRAS = []
-    lstSubjectError = []
-    lstFromRAS = []
-    lstFromError = []
-
-    for i in range(messages, messages - n, -1):
-        res, msg = imap.fetch(str(i), "(RFC822)")
-        for response in msg:
-            if isinstance(response, tuple):
-                msg = email.message_from_bytes(response[1])
-                From = msg["From"]
-                subject = msg["Subject"]
-
-                if "[RAS]" in subject:
-                    lstSubjectRAS.append(subject)
-                    lstFromRAS.append(From)
-                if "[ERREUR]" in subject:
-                    lstSubjectError.append(subject)
-                    lstFromError.append(From)
-
-    def get_context_data(self, **kwargs):
-        context = super(AppMoreInfos, self).get_context_data(**kwargs)
-        context.update(
-            {
-                'lstSubjectError': self.lstSubjectError,
-                'lstSubjectRAS': self.lstSubjectRAS,
-                'lstFromError': self.lstFromError,
-                'lstFromRAS': self.lstFromRAS,
-            }
-        )
-        return context
-
