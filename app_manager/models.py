7ecf39bb12ff6d1e88bd6adac5fac6c182707754
Phenixel <yonathancardoso@outlook.fr>
2022-06-13 16:51:24 +0200

    (update) Best code, Best pratice (fix) errors of subjects

--- app_manager/models.py
+++ app_manager/models.py
@@ -1,4 +1,132 @@
 from django.db import models
+import datetime
+import email
+from email.header import decode_header, make_header
+import imaplib
+import environ
+
+from mail_manager.settings import BASE_DIR
+
+env = environ.Env()
+environ.Env.read_env(env_file=str(BASE_DIR / "mail_manager" / ".env"))
+
+# ----------------------------------------------------- #
+#                Déclarations divers                    #
+# ----------------------------------------------------- #
+encoding = 'utf-8'
+
+# Listes des mots de RAS
+lst_RAS = [
+    "ras",
+    "RAS",
+    "réussi ",
+    "terminée",
+    "terminé"
+]
+
+# Liste des mot d'erreurs
+lst_ERREUR = [
+    "ERREUR",
+    "error",
+    "erreur",
+    "échoué"
+]
+
+# ----------------------------------------------------- #
+#                Récupération des mails                 #
+# ----------------------------------------------------- #
+username = env("USERMAIL")
+password = env("MDPMAIL")
+
+m = imaplib.IMAP4_SSL("imap.gmail.com")
+m.login(username, password)
+
+# Boite
+m.select("inbox")
+m.list()
+
+# Recherche
+date = (datetime.date.today() - datetime.timedelta(30)).strftime("%d-%b-%Y")
+msg_from = '"Yonathan Cardoso"'
+# ----------------------------------------------------- #
 
 
+# Fonction qui decode les chaines de caractére qui viennent des headers des mails
+def decode_str(word):
+    h = make_header(decode_header(word))
+    s = str(h)
+    return s
+
+
+# Fonction qui permet de récupérer les mails choisis pour les sortir dans un dico
+def get_mails():
+    # Déclarations des variables utiles. Ne pas toucher
+    final_list = []
+    lst_mails_ras = []
+    lst_mails_erreurs = []
+
+    # Tri
+    result, data = m.search(None, '(FROM {msg_from} SENTSINCE {date})'.format(date=date, msg_from=msg_from))
+    ids = str(data[0], encoding)
+
+    # Création d'une liste de message par ids
+    id_list = ids.split()
+
+    for emailid in id_list:
+        temp_dict = {}
+        result, data = m.fetch(str(emailid), "(RFC822)")
+        email_body = data[0][1]
+        mail = email.message_from_bytes(email_body)
+
+        temp_dict['Sender'] = mail["From"]
+        temp_dict['Date'] = mail["Date"]
+        s = mail["Subject"]
+        temp_dict['Subject'] = decode_str(s)
+
+        if any(word in temp_dict['Subject'] for word in lst_RAS):
+            lst_mails_ras.append(temp_dict)
+
+        if any(word in temp_dict['Subject'] for word in lst_ERREUR):
+            lst_mails_erreurs.append(temp_dict)
+
+    final_list.append(lst_mails_ras)
+    final_list.append(lst_mails_erreurs)
+
+    return final_list
+
+
+# Fonction qui permet de compter le nombre de RAS et ERREUR
+def count_nb_mails():
+    # Déclarations des variables utiles. Ne pas toucher
+    ras = erreur = 0
+
+    # Tri
+    result, data = m.search(None, '(FROM {msg_from} SENTSINCE {date})'.format(date=date, msg_from=msg_from))
+    ids = str(data[0], encoding)
+
+    # Création d'une liste de message par ids
+    id_list = ids.split()
+
+    for emailid in id_list:
+        temp_dict = {}
+        result, data = m.fetch(str(emailid), "(RFC822)")
+        email_body = data[0][1]
+        mail = email.message_from_bytes(email_body)
+        s = mail["Subject"]
+        temp_dict['Subject'] = decode_str(s)
+
+        if any(word in temp_dict['Subject'] for word in lst_RAS):
+            ras = ras + 1
+
+        if any(word in temp_dict['Subject'] for word in lst_ERREUR):
+            erreur = erreur + 1
+
+    manque = 10 - (ras + erreur)
+    final_nbs = {
+        'ras': ras,
+        'erreur': erreur,
+        'manque': manque
+    }
+
+    return final_nbs
 
--- app_manager/views.py
+++ app_manager/views.py
@@ -1,102 +1,24 @@
-import datetime
-import email
-from email.header import Header, decode_header, make_header
-import imaplib
-import environ
 from django.views.generic import TemplateView
-
-from mail_manager.settings import BASE_DIR
-env = environ.Env()
-environ.Env.read_env(env_file=str(BASE_DIR / "mail_manager" / ".env"))
-
-##########################
-# Récupération des mails #
-##########################
-username = env("USERMAIL")
-password = env("MDPMAIL")
-
-m = imaplib.IMAP4_SSL("imap.gmail.com")
-m.login(username, password)
-
-# Boite
-m.select("inbox")
-m.list()
-
-
-##########################
+from app_manager import models
 
 
 class HomeAppView(TemplateView):
     template_name = "app_manager/index.html"
+    listMails = models.get_mails()
 
-    # Déclarations des variables utiles. Ne pas toucher
-    encoding = 'utf-8'
-    final_list = []
-    ras = erreur = 0
-    lstSubjectRAS = []
-    lstSubjectError = []
-
-    # Listes des mots de RAS
-    lst_RAS = [
-        "ras",
-        "réussi ",
-        "terminée",
-        "terminé"
-    ]
-
-    # Liste des mot d'erreurs
-    lst_ERREUR = [
-        "error",
-        "erreur",
-        "échoué"
-    ]
-
-    # Déclarations
-    attendu = 10
-    date = (datetime.date.today() - datetime.timedelta(30)).strftime("%d-%b-%Y")
-    msg_from = '"Yonathan Cardoso"'
-
-    # Tri
-    result, data = m.search(None, '(FROM {msg_from} SENTSINCE {date})'.format(date=date, msg_from=msg_from))
-    ids = str(data[0], encoding)
-
-    # Création d'une liste de message par ids
-    id_list = ids.split()
-
-    for emailid in id_list:
-        temp_dict = {}
-        result, data = m.fetch(str(emailid), "(RFC822)")
-        email_body = data[0][1]
-        mail = email.message_from_bytes(email_body)
-
-        temp_dict['Sender'] = mail["From"]
-        s = temp_dict['Subject'] = mail["Subject"]
-        temp_dict['Date'] = mail["Date"]
-        pos = temp_dict['Subject'].find('serveur')
-
-        if "[RAS]" in temp_dict['Subject']:
-            ras = ras + 1
-            lstSubjectRAS.append(temp_dict['Subject'][pos:])
-
-        if "[ERREUR]" in temp_dict['Subject']:
-            erreur = erreur + 1
-            lstSubjectError.append(temp_dict['Subject'][pos:])
-
-        # print(temp_dict)
-        final_list.append(temp_dict)
-
-    manque = attendu - (ras + erreur)
-    print(ras)
+    # affine_ras = listMails[0]
+    affine_ras = [sub['Subject'] for sub in listMails[0]]
+    affine_erreur = [sub['Subject'] for sub in listMails[1]]
 
     def get_context_data(self, **kwargs):
         context = super(HomeAppView, self).get_context_data(**kwargs)
         context.update(
             {
-                'ras': self.ras,
-                'erreur': self.erreur,
-                'manque': self.manque,
-                'lstSubjectError': self.lstSubjectError,
-                'lstSubjectRAS': self.lstSubjectRAS,
+                'ras': models.count_nb_mails().get('ras', 'En attente'),
+                'erreur': models.count_nb_mails().get('erreur', 'En attente'),
+                'manque': models.count_nb_mails().get('manque', 'En attente'),
+                'lstSubjectError': self.affine_erreur,
+                'lstSubjectRAS': self.affine_ras,
             }
         )
         return context
